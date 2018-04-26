bddea638a4d399b40f4c4ca6bf1d66450ac029ee
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-04-26 14:38:52 +0700

    Remove variable template

--- examples/timeboard-beical-app/main.tf
+++ examples/timeboard-beical-app/main.tf
@@ -1,5 +1,5 @@
 module "timeboard_api_beical-app" {
   source         = "../../"
-  product_domain = "${var.product_domain}"
-  cluster        = "${var.cluster}"
+  product_domain = "BEI"
+  cluster        = "beical-app"
 }
